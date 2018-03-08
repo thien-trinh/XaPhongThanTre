ef8aed10ebc25026fc1ecaa5237f8e5e669d02d2
thien-trinh <thien.trinh@niteco.se>
2018-03-08 18:23:54 +0700

    fix bug view and missing dlls

---
+++ XPTT.Web/XPTT.Web/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Merchello.Web.Trees;
+using Umbraco.Web;
+
+namespace XPTT.Web
+{
+    public class Global : UmbracoApplication
+    {
+        protected override void OnApplicationStarted(object sender, EventArgs e)
+        {
+            base.OnApplicationStarted(sender, e);
+            var builder = new ContainerBuilder();
+            //builder.RegisterApiControllers(typeof(MerchelloTreeController).Assembly);
+
+            //var container = builder.Build();
+            //DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            //GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+    }
+}
